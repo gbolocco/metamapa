eedc199041043ff040c4cef53ee3e5c8b9ee215b
gianlucca bolocco <gbolocco@frba.utn.edu.ar>
2025-05-15 15:35:08 -0300

    logger

---
+++ src/main/java/ar/edu/utn/frba/dds/AppLogger.java
@@ -0,0 +1,30 @@
+package ar.edu.utn.frba.dds;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppLogger {
+
+  public static Logger getLogger(Class<?> clazz) {
+    return LoggerFactory.getLogger(clazz);
+  }
+}
+
+/*
+Como se usa?
+1. Agregar los siguientes imports a la clase
+import org.slf4j.Logger;
+import ar.edu.utn.frba.dds.AppLogger;
+
+2. Incluir la siguiente linea en la clase que se deseea logear algo
+private static final Logger logger = AppLogger.getLogger(<Mi_CLASE>.class);
+
+NIVELES DE LOGS ( logger.<TIPO_DE_LOG>() )
+debug --> Información útil para desarrolladores en ambiente de desarrollo/test.
+info -->  Mensajes normales, que indican cosas importantes que ocurrieron (sin ser errores).
+warn --> Algo inusual o potencialmente problemático, pero no detiene el sistema.
+error --> Algo falló o no pudo completarse. Deberías investigarlo o manejarlo.
+
+Se genera una carpeta ./logs
+con un archivo metamapa que contiene todo el historial de logs
+*/
--- src/test/java/ar/edu/utn/frba/dds/AplicacionTest.java
+++ src/test/java/ar/edu/utn/frba/dds/AplicacionTest.java
@@ -6,12 +6,28 @@ import ar.edu.utn.frba.dds.dominio.hechos.Hecho;
 import ar.edu.utn.frba.dds.dominio.hechos.OriginHecho;
 import ar.edu.utn.frba.dds.dominio.hechos.Ubicacion;
 import ar.edu.utn.frba.dds.dominio.solicitudes.SolicitudEliminacion;
+import org.slf4j.Logger;
+import ar.edu.utn.frba.dds.AppLogger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 public class AplicacionTest {
+
+  private static final Logger logger = AppLogger.getLogger(AplicacionTest.class);
+
+  @Test
+  public void testLogger(){
+    logger.info("log info");
+    logger.debug("log debug");
+    logger.warn("log warn");
+    logger.error("log error");
+    logger.trace("log trace");
+
+    // para ver los logs de "debug" y "trace" hay que cambiarle el nivel en logback.xml
+    // <root level="DEBUG"> o <root level="TRACE">
+  }
 
   @Test
   @DisplayName("Validaciones de justificacion de solicitud")
