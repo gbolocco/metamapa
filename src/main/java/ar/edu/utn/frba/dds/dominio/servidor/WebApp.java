b85dfb8b67c7df9ddde6137caf53326c3666aa55
melaniesol5 <melaniealarcon97@hotmail.com>
2025-10-23 14:30:29 -0300

    fix error

--- src/main/java/ar/edu/utn/frba/dds/dominio/servidor/WebApp.java
+++ src/main/java/ar/edu/utn/frba/dds/dominio/servidor/WebApp.java
@@ -1,11 +1,7 @@
 package ar.edu.utn.frba.dds.dominio.servidor;
 
-<<<<<<< HEAD
 import static io.javalin.apibuilder.ApiBuilder.get;
 
-=======
-import ar.edu.utn.frba.dds.controladores.ColeccionController;
->>>>>>> ee96926e5b327df28f8964df61e474ebe6c2bfce
 import ar.edu.utn.frba.dds.controladores.HechosController;
 import ar.edu.utn.frba.dds.controladores.LoginController;
 import ar.edu.utn.frba.dds.infraestructura.repositorios.RepositorioUsuarios;
